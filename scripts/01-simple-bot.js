ee8fc355fb0b78795ebf9453a1e5d4712b5f049a
emreahmet07 <161359059+emreahmet07@users.noreply.github.com>
2024-02-26 21:56:04 +0300

    Update 01-simple-bot.js

--- scripts/01-simple-bot.js
+++ scripts/01-simple-bot.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer');
 // Setup bot connection
 const bot = mineflayer.createBot({
   host: 'localhost',
-  port: '12345',
-  username: "Hello_world",
-  version: '1.8.9'
+  port: '53710',
+  username: "emrahmet071",
+  version: '1.16.5'
 });
